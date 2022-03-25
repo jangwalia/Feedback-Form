65504ed81879035c6b4310b5cfbd4927a6d74cd6
jang walia <jangiwalia83@gmail.com>
2022-03-25 07:54:44 -0700

    add reusable Card component

--- src/components/FeedBackItem.jsx
+++ src/components/FeedBackItem.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {FaTimes} from 'react-icons/fa'
+import Card from './shared/Card'
 export default function FeedBackItem({id,text,rating,handleClick}) {
   return (
-    <div className='card'>
+    <Card>
       <div className='num-display'>{rating}</div>
       <div className="close">
         <FaTimes onClick = {()=>handleClick(id)} color = 'purple'/>
       </div>
       <div className='text-display'>{text}</div>
-    </div>
+    </Card>
   )
 }
