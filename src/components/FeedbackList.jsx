cb3477c7158af10caea1b7d2409550cf58669e92
jang walia <jangiwalia83@gmail.com>
2022-03-25 11:05:16 -0700

    install framer-motion for animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import {motion,AnimatePresence} from 'framer-motion'
 import FeedBackItem from './FeedBackItem'
 export default function FeedbackList({feedBack,removeItem}) {
   return (
     <div className= "feedback-list">
+      <AnimatePresence>
       {feedBack.map((item) => (
-        <FeedBackItem handleClick={removeItem} id = {item.id} key = {item.id} text = {item.text} rating = {item.rating}/>
-      ))}
+        <motion.div
+          key = {item.id}
+          initial={{opacity : 0}}
+          animate={{opacity: 1}}
+          exit={{opacity:0}}
+      >
+            <FeedBackItem handleClick={removeItem} id = {item.id} key = {item.id} text = {item.text} rating = {item.rating}/>
+         
+        </motion.div>
+        ))}
+      </AnimatePresence>
+     
       
     </div>
   )
