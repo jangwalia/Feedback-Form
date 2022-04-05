c4877e9357e6b7e81581d880a420860ab0d540cf
jang walia <jangiwalia83@gmail.com>
2022-04-05 05:49:09 -0700

    Add handleclick functions

--- src/components/FeedBackItem.jsx
+++ src/components/FeedBackItem.jsx
@@ -3,11 +3,13 @@ import {FaTimes, FaEdit} from 'react-icons/fa'
 import Card from './shared/Card'
 import '../App.css'
 export default function FeedBackItem({item,handleClick,handleEdit}) {
-
+//this component will be rendered after mapping through the 
+//the feedback list
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
       <div className="close">
+        //Each button triggers the click event
         <button className='close' onClick = {()=>handleClick(item.id)}><FaTimes  color = 'purple'/></button> 
         <button className='edit' onClick = {()=>handleEdit(item)}><FaEdit  color='purple'/></button> 
         
