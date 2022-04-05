604018cf67669681c8c07860f0b04d5ce0aa7649
jang walia <jangiwalia83@gmail.com>
2022-04-05 05:41:33 -0700

    Reusable Card component

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function Card({children}) {
+  //card component which will be used in other components
   return (
     <div className='card'>
       {children}
