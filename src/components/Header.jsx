7776e63b00192f7c4984ceb51142e6918d0954cc
jang walia <jangiwalia83@gmail.com>
2022-04-05 05:54:10 -0700

    Pass props to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Header({text}) {
   return (
     <header>
+      //component use text which is passed as props to show in h2 tag
       <div className="container">
         <h2>{text}</h2>
       </div>
